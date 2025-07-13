70f03571acd148ade60e0d6123ef84bc8733643d
tarmuid <github@tarmuid.io>
2025-07-13 14:10:09 -0700

    tweak(fish,ssh): add ssh-agent

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -3,4 +3,10 @@ source ~/.config/fish/conf.d/paths.fish
 source ~/.config/fish/conf.d/mise.fish
 source ~/.config/fish/conf.d/aliases.fish
 
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
 set -g fish_greeting
